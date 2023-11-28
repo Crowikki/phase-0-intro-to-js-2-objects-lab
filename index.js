2d488072d34400afcbce12cc4f8f2cdb2b901add
+ <+>
2023-11-28 15:46:14 -0600

    KT objects phase-1

--- index.js
+++ index.js
@@ -1,37 +1,33 @@
 // Write your solution in this file!
 
 const employee = {
-   name: "Jeff",
-    streetAddress: "Seaseme street"
+    name: "Sam",
+    streetAddress: "11 Broadway",
+    
 }
 
 function updateEmployeeWithKeyAndValue(employee, key, value){
+     const employeeNew = {...employee};
+     employeeNew[key] = value;
+     return employeeNew;
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value){
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key,) {
     const newEmployee = {...employee};
-    newEmployee[key] = value;
+    //=> this is copying the employee obj and then deleting
+    // the key from the copy 
+    delete newEmployee[key]
     return newEmployee;
 }
-function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
-   employee[key] = value;
-   return employee;
+
+function destructivelyDeleteFromEmployeeByKey(employee, key){
+    
+    delete employee[key]
+    //=> deletes the key value from the obj and returns it with it missing
+    return employee;
 }
-
-function deleteFromEmployeeByKey(employee, key) {
-    const newEmployee = { ...employee };
-    delete newEmployee[key] 
-    // forgot there was a delete keyword to delete but you had everything right
-    // newEmployee = { ...employee}; this line copies the employee object
-    // delete gets rid of the key variable 
-    return newEmployee;
-    // returns the newEmployee variable
-}
-
-function destructivelyDeleteFromEmployeeByKey(employee, key) {
-    const newEmployee = {...employee};
-    // this copies the {employee obj}
-   delete employee[key] // deletes the employee [key] and modifies the original 
-   return employee; // returns the employee obj but deleted the key
-   // don't forget to check this later and watch more tuts on arrays and object manipulation
-
-}
-
-
